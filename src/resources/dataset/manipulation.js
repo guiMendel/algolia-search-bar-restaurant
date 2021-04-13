a7112a27bd71c920e4db73d92d19caff4c2cd6eb
guimendel <guimendeln@gmail.com>
2021-04-13 16:44:50 +0200

    can upload data to algolia index

--- src/resources/dataset/manipulation.js
+++ src/resources/dataset/manipulation.js
@@ -1,8 +1,19 @@
 "use strict";
 
 // Libs
+import dotenv from "dotenv";
+import path from "path";
 import fs from "fs";
 import csv from "csv-parser";
+import algoliasearch from "algoliasearch";
+
+// Get ENV variables (which would be in my gitignore if this weren't an assignment)
+const envPath = path.join(process.cwd(), "..", "..", "..", ".env");
+dotenv.config({ path: envPath });
+
+// Let's get our beautiful algolia index
+const client = algoliasearch(process.env.APP_ID, process.env.API_KEY);
+const index = client.initIndex("restaurants");
 
 // Get base list of restaurants
 const rawData = fs.readFileSync("restaurants_list.json");
@@ -29,18 +40,27 @@ function show() {
 function joinAndPush() {
   // Let's join the records
   for (const restaurant of restaurants) {
+    // Get the additional data counterpart
     const counterpart = additionalData.find(
       // Two =s because one is string and other is number
       (entry) => entry.objectID == restaurant.objectID,
     );
+    if (!restaurant.phone || !counterpart.phone_number)
+      console.log(counterpart);
 
-    // Let's get rid of the string id to avoid overwriting
-    delete counterpart.objectID;
+    // Drop the worst version of duplicated info
+    delete restaurant.phone;
 
+    // Price may be duplicated too, but one version's better to show to the client
+    // and the other is better for ranking purposes, so we'll keep the two
+
+    // Join'em
+    // We want id's as string, so let's have it be overwritten as well
     Object.assign(restaurant, counterpart);
   }
 
   // All's left to do now is push this list to the restaurants index
+  index.saveObjects(restaurants);
 
   show();
 }
