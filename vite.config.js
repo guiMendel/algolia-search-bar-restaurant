2ffb1a5b7f9c328e7e6ed1d73844db638df57f3c
guimendel <guimendeln@gmail.com>
2022-02-14 18:21:59 -0300

    update vite version

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 // export default {
 //   base: "/dist/",
 // }
-export default {
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
   base: "/algolia-search-bar-restaurant/",
-}
+})
